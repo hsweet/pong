71bdab2dcacf9922709a5dec801d3480147574e0
Harry Sweet <hsweet52@gmail.com>
2019-10-30 13:38:08 -0400

    Your draw fuctions are execllent. structure -.
    There is no need for a class, but we never studied that and it made the whole thing confusing.  We can work on that later.  There are simple things that you should do that make code more readable. 
    CONSTANTS are capitalized by convention.  Things like SCREEN_WIDTH.
    Spacing helps as in.
    arcade.draw_line(0 + xPos,0 + yPos,15 + xPos,30 + yPos, arcade.color.BLUE,5)    # more readable
    Good variable names.  I could understand your intent without too much trouble
    I took the mouse stuff out for now.
     arcade.draw_line(15+xPos,30+yPos,30+xPos,0+yPos,arcade.color.BLUE,5)
    camelCase  variables are ok.  I like x_pos better than xPos, both are ok

--- draw2.py
+++ draw2.py
@@ -1,41 +1,28 @@
 import arcade, random
-"""
-Practice basic arcade drawing commands and some practice using the
-arcade api (application programing interface)
-This is a list of all the various arcade functions, methods, classes
-and all the detail you need to program with arcade and Python
- http://arcade.academy/index.html
- http://arcade.academy/quick_index.html def on_mouse_press(self, x, y, button, modifiers):
-        vertical(10,random.randint(10),x,y)
- http://arcade.academy/arcade.color.html
-"""
+ 
 ######## setup stuff ##########################
 
-screen_width = 1280
-screen_height = 960
+SCREEN_WIDTH = 200  #constants should be in CAPS
+SCREEN_HEIGHT = 200
+
+arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Drawing Letter")
+arcade.set_background_color(arcade.color.AMAZON)
+arcade.start_render()
+
+################### end setup #####################
+
 def drawA(xPos,yPos):
-        arcade.draw_line(0+xPos,0+yPos,15+xPos,30+yPos,arcade.color.BLUE,5)
-        arcade.draw_line(15+xPos,30+yPos,30+xPos,0+yPos,arcade.color.BLUE,5)
-        arcade.draw_line(5+xPos,15+yPos,25+xPos,15+yPos,arcade.color.BLUE,5)
+    arcade.draw_line(0 + xPos,0 + yPos,15 + xPos,30 + yPos, arcade.color.BLUE,5)
+    arcade.draw_line(15+xPos,30+yPos,30+xPos,0+yPos,arcade.color.BLUE,5)
+    arcade.draw_line(5+xPos,15+yPos,25+xPos,15+yPos,arcade.color.BLUE,5)
+
 def vertical(amount,times,xPos,yPos):
-        goDown = 0
-        for i in range(0,times):
-                drawA(xPos,yPos-goDown)
-                goDown += amount
-                
-#################################################
-        
-class Drawd(arcade.Window):
+    goDown = 0
+    for i in range(0,times):
+        drawA(xPos,yPos-goDown)
+        goDown += amount
 
+vertical(20, 10, 10, 100)
 
-        def setup(self):
-                arcade.set_background_color(arcade.color.AMAZON)
-        def on_draw(self):
-                arcade.start_render()
-        def on_mouse_press(self, x, y, button, modifiers):
-                print("x=", x, "y=", y, "button=", button)
-                vertical(10,random.randint(2,10),x,y)
-
-game = Drawd(screen_width, screen_height, "Agd;lka")
-game.setup()
+arcade.finish_render()
 arcade.run()   # game loop
