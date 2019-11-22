b8319dac1cb1d504f5a1ec44010b45c34b1dd0b1
Harry Sweet <hsweet52@gmail.com>
2019-11-22 13:19:26 -0500

    fixed minor indentation issue

--- template.py
+++ template.py
@@ -7,30 +7,30 @@ SCREEN_HEIGHT = 600
 class MyGame(arcade.Window):
    """ Main application class. """
 
-   def __init__(self, width, height):
-       super().__init__(width, height)
+    def __init__(self, width, height):
+        super().__init__(width, height)
 
-       arcade.set_background_color(arcade.color.AMAZON)
+        arcade.set_background_color(arcade.color.AMAZON)
 
-   def setup(self):
-       # Set up your game here
-       pass
+    def setup(self):
+        # Set up your game here
+        pass
 
-   def on_draw(self):
-       """ Render the screen. """
-       arcade.start_render()
-       # Your drawing code goes here
+    def on_draw(self):
+        """ Render the screen. """
+        arcade.start_render()
+        # Your drawing code goes here
 
-   def update(self, delta_time):
-       """ All the logic to move, and the game logic goes here. """
-       pass
+    def update(self, delta_time):
+        """ All the logic to move, and the game logic goes here. """
+        pass
 
 
 def main():
-   game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
-   game.setup()
-   arcade.run()
+    game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
+    game.setup()
+    arcade.run()
 
 
 if __name__ == "__main__":
-   main()
+    main()
